89df6f9805efe837c4e01a12ceabb2496db234a6
alihamza00078647-byte <alihamza00078647@gmail.com>
2026-04-05 15:36:29 +0500

    NodeJs Series

---
+++ Lecture 23 Practice Milestone/model/home.js
@@ -0,0 +1,44 @@
+// Database
+// let registeredHomes = [];
+
+
+const path = require('path');
+const fs = require('fs');
+const rootDir = require('../utils/path_Utils');
+const { root } = require('postcss');
+const { register } = require('module');
+
+
+
+module.exports = class houseList{
+    constructor(houseName, price, location, rating, photoUrl){
+        this.houseName = houseName;
+        this.price = price;
+        this.location = location;
+        this.rating = rating;
+        this.photoUrl = photoUrl;
+    }
+
+    save(){
+        houseList.fetchAll((registeredHomes) => {
+            registeredHomes.push(this);
+            const filePath = path.join(rootDir, 'data', 'homesdata.json');
+            fs.writeFile(filePath, JSON.stringify(registeredHomes), (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+    static fetchAll(callback){
+        const filePath = path.join(rootDir, 'data', 'homesdata.json');
+        fs.readFile(filePath, (err, data) => {
+            // callback(!err ? JSON.parse(data): []);
+            if (!err) {
+                callback(JSON.parse(data));
+            } else {
+                // return [];
+                console.log(err);
+            }
+        });
+    }
+}
